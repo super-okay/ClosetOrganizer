5283c518bb2261c5c085bd600906afe4a3e1bc3d
Allen Wang <wangallen98@gmail.com>
2020-05-17 12:08:03 -0500

    initial commit

---
+++ ClosetOrganizer/ClosetItemCustomCell.swift
@@ -0,0 +1,30 @@
+//
+//  ClosetItemCustomCell.swift
+//  ClosetOrganizer
+//
+//  Created by Allen Wang on 5/17/20.
+//  Copyright © 2020 Allen Wang. All rights reserved.
+//
+
+import UIKit
+
+class ClosetItemCustomCell: UITableViewCell {
+
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var brandName: UILabel!
+    @IBOutlet weak var color: UILabel!
+    @IBOutlet weak var lastWorn: UILabel!
+    @IBOutlet weak var purchaseDate: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ClosetOrganizer/ViewController.swift
+++ ClosetOrganizer/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var closetTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
