77fbd89cc0518b4a2486e82a96467f6a3b40dc5a
Allen Wang <wangallen98@gmail.com>
2020-05-17 11:19:50 -0500

    Initial Commit

---
+++ ClosetOrganizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClosetOrganizer
+//
+//  Created by Allen Wang on 5/17/20.
+//  Copyright © 2020 Allen Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
