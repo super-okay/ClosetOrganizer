1b706f33b552554d94aeb61e0ad63a042fe1d81f
Allen Wang <wangallen98@gmail.com>
2020-07-19 22:27:00 -0500

    added edit/delete category popup

---
+++ ClosetOrganizer/DeleteCategoryViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DeleteCategoryViewController.swift
+//  ClosetOrganizer
+//
+//  Created by Allen Wang on 7/19/20.
+//  Copyright © 2020 Allen Wang. All rights reserved.
+//
+
+import UIKit
+
+class DeleteCategoryViewController: UIViewController {
+
+    @IBOutlet var categoryName: UILabel!
+    @IBOutlet var editButton: UIButton!
+    @IBOutlet var deleteButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
